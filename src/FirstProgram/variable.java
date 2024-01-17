ef513a3d692f5a793657fb7d9473b4bfeb37adab
trunghaiii <haitran022001@gmail.com>
2024-01-16 19:30:03 -0500

    Variables

---
+++ src/FirstProgram/variable.java
@@ -0,0 +1,17 @@
+package FirstProgram;
+
+public class variable {
+
+	public static void main(String[] args) {
+
+		String name = "Hai Tran";
+		int age = 22;
+		double gpa = 3.5;
+
+		System.out.println("My name is " + name + ", I am " + age);
+		gpa = 3.6;
+		System.out.println("My GPA is " + gpa);
+
+	}
+
+}
